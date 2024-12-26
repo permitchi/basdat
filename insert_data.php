61fd81ded1ef44d4d71ec032bae7348c81293d7a
permitchi <mickavallery@gmail.com>
2024-12-26 22:53:30 +0700

    Add navigation to switch between index and table + link SQL to table

---
+++ insert_data.php
@@ -0,0 +1,19 @@
+<?php
+    $nama = $_POST['nama'];
+    $nim = $_POST['nim'];
+    $alamat = $_POST['alamat'];
+    $prodi = $_POST['prodi'];
+    $ukt = $_POST['ukt'];
+
+    $conn = new mysqli('localhost', 'root', '', 'basdat');
+    if($conn->connect_error) {
+        die('Connection Error: '.$conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("Insert into siswa(nama,nim,alamat,prodi,ukt)values(?,?,?,?,?)");
+        $stmt->bind_param("sissi", $nama, $nim, $alamat, $prodi, $ukt);
+        $stmt->execute();
+        echo "Input data successfully";
+        $stmt->close();
+        $conn->close();
+    }
+?>
--- tabel_data.php
+++ tabel_data.php
@@ -9,6 +9,7 @@
 <body>
   <div class="container">
     <h1>Data Mahasiswa</h1>
+    <p>Add more data <span><a href="index.html">here</a></span></p>
     <table>
       <thead>
         <tr>
@@ -23,23 +24,24 @@
       <tbody>
         <!-- Contoh data -->
         <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-      </tbody>
-    </table>
+          <?php
+          $conn = mysqli_connect("localhost", "root", '', "basdat");
+          if($conn->connect_error){
+            die("Connection failed : ".$conn->connect_error);
+          }
+
+          $sql = "SELECT id, nama, nim, alamat, prodi, ukt FROM siswa";
+          $result = $conn-> query($sql);
+          
+          if ($result-> num_rows > 0) {
+            while ($row = $result-> fetch_assoc()) {
+              echo "<tr><td>".$row['id']."</td><td>".$row['nama']."</td><td>".$row['nim']."</td><td>".$row['alamat']."</td><td>".$row['prodi']."</td><td>".$row['ukt']."</td></tr>";
+            }
+            echo "</table>";
+          }
+          $conn-> close();
+
+          ?>
   </div>
 
     <div class="query-section">
